191a656f17ea28a636917976c456097ca26d4559
iakss <artemks824@gmail.com>
2025-04-05 18:42:05 +0300

    Added functoins 'AddUser' and 'CustomPlaces'

--- src/sqlite3/db_functions.py
+++ src/sqlite3/db_functions.py
@@ -10,7 +10,7 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(nullable=False)
     email: Mapped[str] = mapped_column(unique=True, nullable=False)
 
@@ -31,6 +31,7 @@ class Place(Base):
 class CustomPlace(Base):
     __tablename__ = "custom_place"
 
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
     name: Mapped[str] = mapped_column(nullable=False)
     address: Mapped[str] = mapped_column(nullable=False)
     desc: Mapped[str] = mapped_column(nullable=True)
@@ -39,13 +40,27 @@ class CustomPlace(Base):
 
     UniqueConstraint(address, fk_user_id)
 
-    __mapper_args__ = {
-        "primary_key": (address, fk_user_id)
-    }
+
+def AddUser(name: str, email: str) -> None:
+    engine = create_engine("sqlite:///database.db")
+    with Session(engine) as session:
+        try:
+            session.add(
+                User(
+                    name=name,
+                    email=email,
+                )
+            )
+            session.flush()
+        except IntegrityError:
+            session.rollback()
+            raise
+        else:
+            session.commit()
 
 
 def AddCustomPlace(name: str, address: str, rate: int, desc: str | None, user_id: int) -> None:
-    engine = create_engine("sqlite:///db.db")
+    engine = create_engine("sqlite:///database.db")
     with Session(engine) as session:
         try:
             session.add(
@@ -65,5 +80,14 @@ def AddCustomPlace(name: str, address: str, rate: int, desc: str | None, user_id
             session.commit()
 
 
-engine = create_engine("sqlite:///db.db")
+def CustomPlaces(page: int, places_per_page: int, user_id: int) -> list[CustomPlace]:
+    engine = create_engine("sqlite:///database.db")
+    with Session(engine) as session:
+        query = session.query(CustomPlace).filter(CustomPlace.fk_user_id == user_id).order_by(CustomPlace.id)
+    query = query.limit(places_per_page).offset((page - 1) * places_per_page)
+    result = query.all()
+    return result
+
+
+engine = create_engine("sqlite:///database.db")
 Base.metadata.create_all(engine)
