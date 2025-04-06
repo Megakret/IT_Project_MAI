b51dae419a151ca9c855b4176dacb99e61d86962
NoyerXoper <chudov_2006@mail.ru>
2025-04-06 11:52:18 +0300

    OOP is back

--- src/api/gpt/GptRequest.py
+++ src/api/gpt/GptRequest.py
@@ -1,14 +1,16 @@
 from httpx import AsyncClient, Response
 from os import getenv
+from dotenv import load_dotenv
 
+class GptRequest:
+    def __init__(self):
+        self._indentification_key: str = getenv("GPT_INDENTIFICATION")
+        self._api_key: str = getenv("GPT_API_KEY")
+        self._url: str = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
+        self._headers: dict = {
+            "Content-Type": "application/json",
+            "Authorization": f"Api-Key {self._api_key}"
+        }
 
-_indentification_key: str = getenv("GPT_INDENTIFICATION")
-_api_key: str = getenv("GPT_API_KEY")
-_url: str = "https://llm.api.cloud.yandex.net/foundationModels/v1/completion"
-_headers: dict = {
-    "Content-Type": "application/json",
-    "Authorization": f"Api-Key {_api_key}"
-}
-
-async def request(client: AsyncClient, prompt: dict) -> Response:
-    return await client.post(_url, headers=_headers, json=prompt)
+    async def request(self, client: AsyncClient, prompt: dict) -> Response:
+        return await client.post(self._url, headers=self._headers, json=prompt)
--- src/api/gpt/GptSummarize.py
+++ src/api/gpt/GptSummarize.py
@@ -1,29 +1,28 @@
-from GptRequest import request, _indentification_key
-import asyncio
+from api.gpt.GptRequest import GptRequest
 from httpx import AsyncClient
 import json
 from copy import deepcopy
 
 
-__prompt: dict = None
-with open("src/api/gpt/review_prompt.json") as file:
-    __prompt = json.load(file)
-    __prompt["modelUri"] = f"gpt://{_indentification_key}/yandexgpt-lite/latest"
+class GptSummarize(GptRequest):
+    def __init__(self):
+        super().__init__()
+        with open("src/api/gpt/review_prompt.json") as file:
+            self.__prompt = json.load(file)
+            self.__prompt["modelUri"] = f"gpt://{self._indentification_key}/yandexgpt-lite/latest"
 
-def __construct_message(reviews: list[str]) -> str:
-    s = "Сделай выжимку из данных отзывов о местах:\n\n"
-    for i, l in enumerate(reviews):
-        s += f"{i + 1}. {l}\n\n"
-    return s
+    def __construct_message(reviews: list[str]) -> str:
+        s = "Сделай выжимку из данных отзывов о местах:\n\n"
+        for i, l in enumerate(reviews):
+            s += f"{i + 1}. {l}\n\n"
+        return s
 
-async def summarize(client: AsyncClient, reviews: list[str]) -> str:
-    prompt = deepcopy(__prompt)
-    prompt["messages"].append({"role": "user", "text": __construct_message(reviews)})
-    response = await request(client, prompt)
-    return response.json()["result"]["alternatives"][0]["message"]["text"]
+    async def summarize(self, client: AsyncClient, reviews: list[str]) -> str:
+        prompt = deepcopy(self.__prompt)
+        prompt["messages"].append({"role": "user", "text": GptSummarize.__construct_message(reviews)})
+        response = await self.request(client, prompt)
+        return response.json()["result"]["alternatives"][0]["message"]["text"]
 
-async def summarize_NAC(reviews: list[str]) -> str:
-    async with AsyncClient() as client:
-        return await summarize(client, reviews)
-
-
+    async def summarize_NAC(self, reviews: list[str]) -> str:
+        async with AsyncClient() as client:
+            return await self.summarize(client, reviews)
