4179db0ce2e67cab3e601c3c75d74371e227dd92
Megakret <tymatee@yandex.ru>
2025-04-17 00:39:15 +0300

    fixed user place list

--- src/tg_bot/routers/user_place_list_handler.py
+++ src/tg_bot/routers/user_place_list_handler.py
@@ -4,7 +4,7 @@ from aiogram.filters import Command
 from aiogram.fsm.context import FSMContext
 from sqlalchemy.ext.asyncio import AsyncSession
 from tg_bot.ui_components.Paginator import PaginatorService
-from database.db_functions import get_user_places, UserPlace
+from database.db_functions import get_user_places, Place
 from tg_bot.keyboards import NEXT_PAGE, PREV_PAGE, INDICATOR_CLICKED
 
 router = Router()
@@ -14,10 +14,12 @@ POSTFIX = "user_place"
 async def get_formatted_list(
     page: int, places_per_page: int, session: AsyncSession, user_id: int, **kwargs
 ) -> str:
-    place_list: list[UserPlace] = await get_user_places(session, page, places_per_page, user_id)
-    place_formatted_list: map[str] = map(
-        lambda x: f"{x.fk_place_address}\n{x.score}\n", place_list
-    )
+    place_list: list[tuple[Place | int | None]] = await get_user_places(session, page, places_per_page, user_id)
+    place_formatted_list: list[str] = []
+    for item in place_list:
+        place: Place = item[0]
+        score: int = item[1]
+        place_formatted_list.append(f"{place.name}\n{place.address}\n{place.desc}\nВаша оценка: {score}")
     return place_formatted_list
 
 
