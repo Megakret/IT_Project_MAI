d55696ade5413d1c2c5aa74c48937d9b2ff9a8ee
NoyerXoper <chudov_2006@mail.ru>
2025-05-14 18:31:39 +0300

    all

--- src/database/db_functions.py
+++ src/database/db_functions.py
@@ -1,10 +1,11 @@
 import asyncio
 import sqlite3
-
 from sqlalchemy import (
     CheckConstraint,
     ForeignKey,
     UniqueConstraint,
+    delete,
+    exists,
     func,
     select,
     update,
@@ -41,32 +42,41 @@ class Base(AsyncAttrs, DeclarativeBase):
 class User(Base):
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(unique=True, autoincrement=False)
-    name: Mapped[str]
-    email: Mapped[str] = mapped_column(unique=True)
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=False)
+    name: Mapped[str] = mapped_column(unique=True)
+    rights: Mapped[int] = mapped_column(CheckConstraint("rights BETWEEN 1 and 3"))
+    is_banned: Mapped[bool]
 
     user_places: Mapped[list["UserPlace"]] = relationship(
-        backref="user", cascade="all, delete-orphan"
+        back_populates="parent_user", cascade="all, delete-orphan"
     )
-
-    __mapper_args__ = {"primary_key": id}
 
 
 class Place(Base):
     __tablename__ = "place"
 
+    id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
     address: Mapped[str] = mapped_column(unique=True)
     desc: Mapped[str | None]
 
     user_places: Mapped[list["UserPlace"]] = relationship(
-        backref="place", cascade="all, delete-orphan"
+        back_populates="parent_place", cascade="all, delete-orphan"
     )
     place_tags: Mapped[list["Tag"]] = relationship(
-        backref="place", cascade="all, delete-orphan"
+        back_populates="parent_place", cascade="all, delete-orphan"
     )
 
-    __mapper_args__ = {"primary_key": address}
+
+class Requests(Base):
+    __tablename__ = "requests"
+
+    fk_user_id: Mapped[int] = mapped_column(ForeignKey(User.id))
+    name: Mapped[str]
+    address: Mapped[str] = mapped_column(unique=True)
+    desc: Mapped[str | None]
+
+    __mapper_args__ = {"primary_key": [fk_user_id, address]}
 
 
 class UserPlace(Base):
@@ -76,6 +86,9 @@ class UserPlace(Base):
     fk_place_address: Mapped[str] = mapped_column(ForeignKey(Place.address))
     score: Mapped[int | None] = mapped_column(CheckConstraint("score BETWEEN 1 and 10"))
     comment: Mapped[str | None]
+
+    parent_user: Mapped["User"] = relationship(back_populates="user_places")
+    parent_place: Mapped["Place"] = relationship(back_populates="user_places")
 
     UniqueConstraint(fk_user_id, fk_place_address)
 
@@ -88,18 +101,27 @@ class Tag(Base):
     fk_place_address: Mapped[str] = mapped_column(ForeignKey(Place.address))
     place_tag: Mapped[str] = mapped_column()
 
+    parent_place: Mapped["Place"] = relationship(back_populates="place_tags")
+
     UniqueConstraint(fk_place_address, place_tag)
 
     __mapper_args__ = {"primary_key": [fk_place_address, place_tag]}
 
 
-async def add_user(session: AsyncSession, id: int, name: str, email: str) -> None:
+async def add_user(
+    session: AsyncSession,
+    id: int,
+    name: str,
+    rights: int | None = 1,
+    is_banned: bool | None = False,
+) -> None:
     try:
         session.add(
             User(
                 id=id,
                 name=name,
-                email=email,
+                rights=rights,
+                is_banned=is_banned,
             )
         )
         await session.flush()
@@ -107,9 +129,9 @@ async def add_user(session: AsyncSession, id: int, name: str, email: str) -> Non
         await session.rollback()
         if isinstance(error.orig, sqlite3.IntegrityError):
             if error.orig.sqlite_errorcode == 2067:
-                raise UniqueConstraintError(["email"], [email])
+                raise UniqueConstraintError(["id"], [str(id)])
             else:
-                raise ConstraintError(["email"], [email])
+                raise ConstraintError(["id"], [str(id)])
         else:
             raise
     except:
@@ -117,6 +139,74 @@ async def add_user(session: AsyncSession, id: int, name: str, email: str) -> Non
         raise
     else:
         await session.commit()
+
+
+async def delete_user(session: AsyncSession, user_id: int) -> None:
+    result = await session.execute(
+        delete(User).where(User.id == user_id).returning(User.id)
+    )
+
+    if not result.scalar_one_or_none():
+        raise ValueError(f"User with ID {user_id} not found")
+
+    await session.commit()
+
+
+async def is_existing_user(session: AsyncSession, id: int) -> bool:
+    statement = select(exists().where(User.id == id))
+    is_existing = await session.execute(statement)
+    return is_existing.scalar_one()
+
+
+async def is_manager(session: AsyncSession, id: int) -> bool:
+    result = await session.execute(statement=select(User.rights).where(User.id == id))
+    return result.scalar_one() >= 2
+
+
+async def is_admin(session: AsyncSession, id: int) -> bool:
+    result = await session.execute(statement=select(User.rights).where(User.id == id))
+    return result.scalar_one() == 3
+
+
+async def make_manager(session: AsyncSession, username: str) -> None:
+    result = await session.execute(select(User).where(User.name == username))
+    user = result.scalar_one_or_none()
+
+    if user is None:
+        raise ValueError(f"User '{username}' not found")
+    user.rights = 2
+    await session.commit()
+
+
+async def make_admin(session: AsyncSession, username: str) -> None:
+    result = await session.execute(select(User).where(User.name == username))
+    user = result.scalar_one_or_none()
+
+    if user is None:
+        raise ValueError(f"User '{username}' not found")
+    user.rights = 3
+    await session.commit()
+
+
+async def ban(session: AsyncSession, id: int) -> None:
+    await session.execute(
+        statement=update(User).where(User.id == id).values(is_banned=True)
+    )
+
+
+async def unban(session: AsyncSession, id: int) -> None:
+    await session.execute(
+        statement=update(User).where(User.id == id).values(is_banned=False)
+    )
+
+
+async def is_user_banned(session: AsyncSession, user_id: int) -> bool:
+    result = await session.execute(select(User.is_banned).where(User.id == user_id))
+    banned_status = result.scalar_one_or_none()
+
+    if banned_status is None:
+        return False
+    return banned_status
 
 
 async def add_place(
@@ -150,6 +240,16 @@ async def add_place(
         await session.commit()
 
 
+async def delete_place_by_address(session: AsyncSession, address: str) -> None:
+    result = await session.execute(select(Place).where(Place.address == address))
+    place = result.scalar_one_or_none()
+
+    if place is None:
+        raise ValueError(f"Place with address '{address}' not found")
+    await session.execute(delete(Place).where(Place.address == address))
+    await session.commit()
+
+
 async def get_places(
     session: AsyncSession, page: int, places_per_page: int
 ) -> list[Place]:
@@ -162,6 +262,16 @@ async def get_places(
     result = await session.execute(statement)
     instance_list = list(result.scalars().all())
     return instance_list
+
+
+async def is_existing_place(session: AsyncSession, address: str) -> bool:
+    statement = select(exists().where(Place.address == address))
+    is_existing = await session.execute(statement)
+    return is_existing.scalar_one()
+
+
+async def remove_place(session: AsyncSession, address: str) -> None:
+    await session.execute(statement=delete(Place).where(Place.address == address))
 
 
 async def add_place_tag(session: AsyncSession, address: str, place_tag: str) -> None:
@@ -191,6 +301,33 @@ async def add_place_tag(session: AsyncSession, address: str, place_tag: str) -> 
         await session.commit()
 
 
+async def add_place_tags(
+    session: AsyncSession, address: str, place_tags: tuple[str]
+) -> None:
+    try:
+        instances_to_add = [
+            Tag(fk_place_address=address, place_tag=tag) for tag in place_tags
+        ]
+        session.add_all(instances_to_add)
+        await session.flush()
+    except IntegrityError as error:
+        await session.rollback()
+        if isinstance(error.orig, sqlite3.IntegrityError):
+            if error.orig.sqlite_errorcode == 2067:
+                raise UniqueConstraintError(
+                    ["address", "place_tags"], [address, *place_tags]
+                )
+            else:
+                raise ConstraintError(["address", "place_tags"], [address, *place_tags])
+        else:
+            raise
+    except:
+        await session.rollback()
+        raise
+    else:
+        await session.commit()
+
+
 async def get_place_tags(session: AsyncSession, address: str) -> tuple[str]:
     statement = select(Tag.place_tag).where(Tag.fk_place_address == address)
     result = await session.execute(statement)
@@ -198,9 +335,14 @@ async def get_place_tags(session: AsyncSession, address: str) -> tuple[str]:
     return tags_list
 
 
-async def get_places_with_tag(session: AsyncSession, tag: str) -> list[Place]:
-    statement = select(Place).where(
-        Tag.place_tag == tag, Place.address == Tag.fk_place_address
+async def get_places_with_tag(
+    session: AsyncSession, tag: str, page: int, places_per_page: int
+) -> list[Place]:
+    statement = (
+        select(Place)
+        .where(Tag.place_tag == tag, Place.address == Tag.fk_place_address)
+        .limit(places_per_page)
+        .offset((page - 1) * places_per_page)
     )
     result = await session.execute(statement)
     instance_list = list(result.scalars().all())
@@ -242,6 +384,22 @@ async def rate(
         await session.commit()
 
 
+async def user_has_comment(
+    session: AsyncSession, user_id: int, place_address: str
+) -> bool:
+    comment_exists = await session.execute(
+        select(
+            exists().where(
+                UserPlace.fk_user_id == user_id,
+                UserPlace.fk_place_address == place_address,
+                UserPlace.comment.is_not(None),
+            )
+        )
+    )
+
+    return comment_exists.scalar_one()
+
+
 async def add_comment(
     session: AsyncSession,
     user_id: int,
@@ -277,29 +435,37 @@ async def add_comment(
         await session.commit()
 
 
+# TODO: I need username which starts with @, not plain integer user id
+# returns <username with @, comment, score>
 async def get_place_comments(
     session: AsyncSession, page: int, comments_per_page: int, address: str
-) -> list[str | None] | None:
+) -> list[tuple[str, str, int]]:
     statement = (
-        select(UserPlace.comment)
+        select(User.name, UserPlace.comment, UserPlace.score)
+        .join(User, User.id == UserPlace.fk_user_id)
         .where(UserPlace.fk_place_address == address, UserPlace.comment.is_not(None))
+        .order_by(UserPlace.score.desc())
         .limit(comments_per_page)
         .offset((page - 1) * comments_per_page)
     )
+
     result = await session.execute(statement)
-    # comments_list = list(result.scalars().all()) if result.scalar_one_or_none() else None
-    return list(result.scalars().all())
+    comments = result.all()
+    return [
+        ("@" + username, comment, score)
+        for username, comment, score in comments
+        if comment is not None
+    ]
 
 
 async def get_place_comments_all(
     session: AsyncSession, address: str
-) -> list[str | None] | None:
-    statement = select(UserPlace.comment).where(
+) -> list[tuple[int, str | None]]:
+    statement = select(UserPlace.fk_user_id, UserPlace.comment).where(
         UserPlace.fk_place_address == address, UserPlace.comment.is_not(None)
     )
     result = await session.execute(statement)
-    # comments_list = list(result.scalars().all()) if result.scalar_one_or_none() else None
-    return list(result.scalars().all())
+    return list(result.tuples())
 
 
 async def add_user_place(
@@ -316,10 +482,10 @@ async def add_user_place(
         await session.flush()
         if score:
             await rate(session, user_id, address, score)
-        await session.flush()
+            await session.flush()
         if comment:
             await add_comment(session, user_id, address, comment)
-        await session.flush()
+            await session.flush()
     except IntegrityError as error:
         await session.rollback()
         if isinstance(error.orig, sqlite3.IntegrityError):
@@ -368,6 +534,28 @@ async def get_place_with_score(
     result = await session.execute(statement)
     instance_with_score = result.one().tuple()
     return instance_with_score
+
+
+async def remove_review(session: AsyncSession, username: str, place_id: int) -> None:
+    statement = delete(UserPlace).where(
+        UserPlace.fk_user_id
+        == (
+            (
+                await session.execute(
+                    statement=select(User.id).where(User.name == username)
+                )
+            ).scalar_one()
+        ),
+        UserPlace.fk_place_address
+        == (
+            (
+                await session.execute(
+                    statement=select(Place.address).where(Place.id == place_id)
+                )
+            ).scalar_one()
+        ),
+    )
+    await session.execute(statement)
 
 
 async def async_main() -> None:
